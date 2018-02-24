9aac039ebd401d423256bd52c294d46185d3e1e7
Lloric Mayuga Garcia <lloricode@gmail.com>
2018-02-24 16:15:50 +0800

    class
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+require __DIR__.'/vendor/autoload.php';
+
+echo "==================\n";
+echo "==================\n";
+echo "    calculator\n";
+echo "==================\n";
+echo "==================\n";
---
+++ src/Calculator.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Lloricode\Calculator;
+
+class Calculator
+{
+
+}
