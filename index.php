817c026a8e7b56c1df577775d196dd6a43d79997
Lloric Mayuga Garcia <lloricode@gmail.com>
2018-02-24 16:23:41 +0800

    param
    Signed-off-by: Lloric Mayuga Garcia <lloricode@gmail.com>

--- index.php
+++ index.php
@@ -6,3 +6,13 @@ echo "==================\n";
 echo "    calculator\n";
 echo "==================\n";
 echo "==================\n";
+
+if (!defined('STDIN')) {
+        echo 'STDIN required' . "\n";
+        die();
+}
+
+$input1 = $argv[1];
+$input2 = $argv[2];
+
+print_r($argv);
